09f9e97b3d96dde2fdbb7911d6076c3e001829eb
atulendrasinghyadav <atulendrasinghyadav007@gmail.com>
2026-01-13 23:50:05 +0530

    Tried ques on 13/01/2026

---
+++ BeautifulMatrix.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class BeautifulMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[][] = new int[5][5];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[i][j] == 1) {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+
+        int result = Math.abs(x - 2) + Math.abs(y - 2);
+        System.out.println(result);
+    }
+}
