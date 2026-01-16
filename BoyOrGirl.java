47786fdbf654e4c53d09b8e29294324db7aa6dd5
atulendrasinghyadav <atulendrasinghyadav007@gmail.com>
2026-01-17 00:04:42 +0530

    Done Boy Or Girl on 16.01.2026

--- BoyOrGirl.java
+++ BoyOrGirl.java
@@ -1,5 +1,21 @@
+import java.util.*;
 public class BoyOrGirl {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        Set<Character> charSet = new HashSet<>();
+
+        for (char temp : str.toCharArray()) {
+            charSet.add(temp);
+        }
+
+        if (charSet.size() % 2 != 0) {
+            System.out.println("IGNORE HIM!");
+        }
+        else{
+            System.out.println("CHAT WITH HER!");
+        }
     }
 }
