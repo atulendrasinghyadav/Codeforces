d5c861f9d4fece1bb9c6313b48e4f83ae7be0238
atulendrasinghyadav <atulendrasinghyadav007@gmail.com>
2026-01-29 21:48:14 +0530

    Done Beautiful Year on 29/01/2026

---
+++ BeautifulYear.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BeautifulYear{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (;;) {
+            n++;
+            String year = Integer.toString(n);
+            if (year.charAt(0) != year.charAt(1) && year.charAt(0) != year.charAt(2) && year.charAt(0) != year.charAt(3) &&
+                year.charAt(1) != year.charAt(2) && year.charAt(1) != year.charAt(3) &&
+                year.charAt(2) != year.charAt(3)) {
+                System.out.println(year);
+                break;
+            }
+        }
+    }
+}
